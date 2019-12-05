3d46cc880b99615addf548860f1d69b2ad700186
AMasel <40176679+MaslaLT@users.noreply.github.com>
2019-12-05 11:29:56 +0200

    Front side fully finished.

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -15,3 +15,56 @@ function changeLoginToRegistrationForm() {
     document.getElementById('loginIndicator').style.display= "block" ;
 
 }
+
+function validateFormRegistration() {
+    var name = document.forms["registrationForm"]["firstName"].value;
+    var mail = document.forms["registrationForm"]["email"].value;
+    var pass = document.forms["registrationForm"]["password"].value;
+
+    if (name || mail || pass == "") {
+        if (name == "") {
+            document.getElementById('registrationFormUsernameCheckbox').style.background = "red";
+        } else {
+            document.getElementById('registrationFormUsernameCheckbox').style.background = "green";
+        }
+        if (mail == "") {
+            document.getElementById('registrationFormEmailCheckbox').style.background = "red";
+        } else {
+            document.getElementById('registrationFormEmailCheckbox').style.background = "green";
+        }
+        if (pass == "") {
+            document.getElementById('registrationFormPasswordCheckbox').style.background = "red";
+        } else {
+            document.getElementById('registrationFormPasswordCheckbox').style.background = "green";
+        }
+        return false;
+    }
+}
+
+function validateFormLogin() {
+    var mail = document.forms["registrationForm"]["email"].value;
+    var pass = document.forms["registrationForm"]["password"].value;
+
+    if (name || mail || pass == "") {
+        if (name == "") {
+            document.getElementById('loginFormUsernameCheckbox').style.background = "red";
+        } else {
+            document.getElementById('loginFormUsernameCheckbox').style.background = "green";
+        }
+        if (mail == "") {
+            document.getElementById('loginFormEmailCheckbox').style.background = "red";
+        } else {
+            document.getElementById('loginFormEmailCheckbox').style.background = "green";
+        }
+        if (pass == "") {
+            document.getElementById('loginFormPasswordCheckbox').style.background = "red";
+        } else {
+            document.getElementById('loginFormPasswordCheckbox').style.background = "green";
+        }
+        return false;
+    }
+}
+
+function navbarLoginPopup(){
+    document.getElementById('loginFormUsernameCheckbox').style.background = "red";
+}
